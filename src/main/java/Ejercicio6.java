abb05c56d4972bc38b8af35ef4675fe10de15587
ana <andelamo@myuax.com>
2023-02-15 22:47:30 +0100

    Que hace este algoritmo

---
+++ src/main/java/Ejercicio6.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Ejercicio6 {
+    /*Observe atentamente el siguiente algoritmo:
+
+Algo Trampa
+Variable numIntento : entero <- 1
+Variable valorIntroducido : texto
+Constante MAX_INTENTOS : entero <- 5
+Inicio
+   valorIntroducido <- introducir("¿Cuál es la capital de Francia? ")
+   MientrasQue valorIntroducido ≠ "París" o MAX_INTENTOS - numIntentos ≠ 0
+         escribir("Respuesta incorrecta")
+         escribir("Solo quedan " & MAX_INTENTOS - numIntentos &
+" intento(s)")
+         valorIntroducido <- introducir("¿Cuál es la capital de Francia? ")
+   FMq
+   Si MAX_INTENTOS - numIntento ≠ 0 Entonces
+         escribir("Bravo")
+   Si no
+         escribir("Revise sus conocimientos en geografía")
+   FSi
+Fin
+¿Qué hace?
+
+Corrija este algoritmo para que logre lo que se podría esperar.*/
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        // TODO code application logic here
+        int numIntento = 1;
+        String valorIntroducido;
+        final int MAX_INTENTOS = 5;
+        System.out.println("¿Cuál es la capital de Francia? ");
+        valorIntroducido = teclado.nextLine();
+        while (!valorIntroducido.equals("París") && MAX_INTENTOS - numIntento != 0) {
+            System.out.println("Respuesta incorrecta");
+            System.out.println("Solo quedan " + (MAX_INTENTOS - numIntento) + " intento(s)");
+            System.out.println("¿Cuál es la capital de Francia? ");
+            valorIntroducido = teclado.nextLine();
+            numIntento++;
+        }
+        if (MAX_INTENTOS - numIntento != 0) {
+            System.out.println("Bravo");
+        } else {
+            System.out.println("Revise sus conocimientos en geografía");
+        }
+    }
+}
